9e96033af09cc5500f0be85fd584dfcaf7d8fd5e
Zagg2732 <Zagg2732@gmail.com>
2020-09-18 18:13:34 +0900

    새로배운것들 활용가능한지 여부

--- Lists.py
+++ Lists.py
@@ -1,10 +1,10 @@
 import os
-import datetime
-import re
 import Dday
 
 ## DailyList는 매일 할일(routine)식으로 저장됩니다.
 
+text_types = ["D-Day.txt", "Daily.txt", "Today.txt"]
+done_txt_types = ["DoneD-Day.txt", "DoneDaily.txt", "DoneToday.txt"]
 
 def set_list () :
 
@@ -16,21 +16,13 @@ def set_list () :
     elif number == 3 :
         txt_name = "Today.txt"
 
-    #warning = input("이 코드를 실행하면 기존에 저장된 %s의 내용물이 사라지고 다시 입력합니다. 그래도 계속 하시겠습니까? (Y/N)\n" % txt_name)
-    #if warning == "Y":
-    open_list = open(txt_name , 'r', encoding='utf-8')
-    check_list = []
-
-    for line in open_list:
-        data = line.strip()
-        check_list.append(data)
-
+    check_list = open_txt(txt_name)
 
     input_command = None
 
     while input_command != "exit":
-        input_command = input("%s 리스트를 불러왔습니다. %s \n add : 리스트 추가 list : 리스트 출력 \nundo : 추가한 리스트 "
-                              "취소 del : 특정 리스트 삭제 exit : 저장 및 뒤로가기\n>>>> " %(txt_name, check_list))
+        input_command = input("%s 리스트를 불러왔습니다. %s \n add : 리스트 추가 list : 리스트 출력 \nundo : 마지막 할일 삭제 "
+                              " del : 특정 할일 삭제 clear : 할일 모두 삭제 \nexit : 저장 및 뒤로가기\n>>>> " %(txt_name, check_list))
         if input_command == "add":
             if txt_name == "D-Day.txt" :
                 check_list.append(Dday.set_dday("D-Day.txt"))
@@ -46,16 +38,14 @@ def set_list () :
             check_list.pop(done - 1)
             print(check_list)
 
+        elif input_command == "clear" :
+            check_list = []
 
         elif input_command == "undo":
             check_list.pop()
 
         elif input_command == "exit":
-            write_list = open(txt_name, 'w', encoding='utf-8')
-            for i in check_list:
-                write_list.write(i + "\n")
-            print("리스트 만들기 종료")
-            print("저장 경로 -> " + os.getcwd() + "\\" + txt_name)
+            write_text(txt_name, check_list)
 
         else:
             print(input_command + " 명령어가 없습니다 다시 입력해주세요")
@@ -71,9 +61,9 @@ def load_txt(txt_name) :
         print(x.strip())
 
 def lists() :
-    print("List 목록을 불러옵니다.")
+    print("경로 %s 의 To Do List 목록을 불러옵니다." % str(os.getcwd()))
     file_list = os.listdir("./")
-    txt_list = [x for x in file_list if x.endswith(".txt")] #경로내의 모든 텍스트 폴더 찾기
+    txt_list = [x for x in file_list if x.endswith(".txt") and not x.startswith('Done')] #경로내의 모든 텍스트 폴더 찾기
     i = 1
     for x in txt_list :
         print(str(i) + "번 : " + x)
@@ -81,3 +71,37 @@ def lists() :
     for x in txt_list :
         load_txt(x)
 
+
+def open_txt(txt_name):
+    open_list = open(txt_name, 'r', encoding='utf-8')
+    check_list = []
+    for line in open_list:
+        data = line.strip()
+        check_list.append(data)
+    open_list.close()
+    return check_list
+
+def current_list(txt_name) :
+    done = set(open_txt("Done" + str(txt_name)))
+    check = set(open_txt(txt_name))
+    return list(check - done)
+
+def write_text(txt_name, check_list) :
+    writing = open(txt_name, 'w', encoding='utf-8')
+    for i in check_list:
+        writing.write(i + "\n")
+    print("저장 경로 -> " + os.getcwd() + "\\" + txt_name)
+
+def choose() :
+    try :
+        number = int(input("리스트를 입력해주세요 1. D-day 2. Daily 3. Today\n>>>> "))
+    except:
+        print("잘못된 입력값입니다. 숫자만 입력해 주세요")
+        number = int(input("리스트를 입력해주세요 1. D-day 2. Daily 3. Today\n>>>> "))
+    if number == 1:
+        txt_name = "D-Day.txt"
+    elif number == 2:
+        txt_name = "Daily.txt"
+    elif number == 3:
+        txt_name = "Today.txt"
+    return txt_name
--- main.py
+++ main.py
@@ -1,17 +1,59 @@
 import Lists
 import os
+
+
 command = ""
 Lists.lists()
-
-
+print("-----------------")
 while command != "exit" :
-    command = input("커맨드입력 . 리스트 선택 및 수정 : set 종료 : exit \n>>>> ")
+    command = input("커맨드입력 \n리스트 선택 및 수정 : set \n수정하지 않고 넘어가기 : pass \n>>>> ")
 
     if command == "set" :
         Lists.set_list()
 
-    elif command == "exit" :
+    elif command == "pass" :
         break
+
+command = ""
+while command != "exit" :
+    command = input("커맨드입력\n오늘 한일 : done 남은 일만 보기 : remain \n한일 초기화 : clear 한일 모두 초기화 : clearA\n>>>> ")
+
+    if command == "done" :
+        txt_name = Lists.choose()
+        print(txt_name)
+        check_list = Lists.open_txt(txt_name)
+        print(check_list)
+        done = input("몇 번째 할일을 했습니까?       뒤로가기 : undo\n>>>> ")
+        if done == "undo" :
+            pass
+        else:
+            done = int(done)
+            print(check_list[done - 1] + " 가 한일에 추가됩니다.")
+            done_list = Lists.open_txt("Done" + str(txt_name))
+            done_list.append(check_list[done - 1])
+            Lists.write_text("Done" + str(txt_name),done_list)
+
+    elif command == "remain" :
+        txt_name = Lists.choose()
+        print(Lists.current_list(txt_name))
+
+    elif command == "clear" :
+        check_list = []
+        txt_name = Lists.choose()
+        Lists.write_text("Done"+str(txt_name),check_list)
+
+    elif command == "clearA" :
+        check_list = []
+        for i in range(Lists.done_txt_types) :
+            erase = open(i, w, encoding= 'utf-8')
+            erase.close()
+    else:
+        print("명령어를 잘못 입력하셨습니다.")
+
+
+
+
+
 
 
 
